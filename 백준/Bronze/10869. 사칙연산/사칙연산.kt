528b48189abf488c9f4428b586fb173ff4a668de
KimDain <80109656+ekdls278@users.noreply.github.com>
2023-03-27 14:14:16 +0900

    [Bronze V] Title: 사칙연산, Time: 116 ms, Memory: 13252 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)){
+    var A = nextInt()
+    var B = nextInt()
+
+    println(A+B)
+    println(A-B)
+    println(A*B)
+    println(A/B)
+    println(A%B)
+}
