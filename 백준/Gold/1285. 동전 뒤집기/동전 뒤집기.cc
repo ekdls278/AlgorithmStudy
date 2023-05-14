04b40bf0caaee3200553fc9bee6076649d8eaa3f
KimDain <80109656+ekdls278@users.noreply.github.com>
2023-05-14 14:22:30 +0900

    [Gold I] Title: 동전 뒤집기, Time: 336 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -1,54 +1,60 @@
 #include <iostream>
-#include<algorithm>
-#define MAX 44
-#define INF	1987654321
+#include <algorithm>
+#define MAX 22
+#define INF 1987654321
 using namespace std;
 
 int N;
-int coin[MAX];
-string str;
+int arr[MAX];
 int ret = INF;
-void solution(int here)
+string str;
+
+void dfs(int cur)
 {
-	if (here == N+1)
+	if (cur == N)
 	{
 		int sum = 0;
-		for (int i = 1; i <= (1 << (N - 1)); i *= 2)
+		//열 뒤집을지 말지 확인
+		for (int i = 1; i <= (1 << (N-1)); i*=2)
 		{
 			int cnt = 0;
-			for (int j = 1; j <= N; j++)if (coin[j] & i) cnt++;
+			for (int j = 0; j < N; j++)
+			{
+				//T이면
+				if (arr[j] & i)cnt++;
+			}
 			sum += min(cnt, N - cnt);
+
 		}
 		ret = min(ret, sum);
 		return;
 	}
 
-	//here 행 뒤집었을 때
-	coin[here] = ~coin[here];
-	solution(here + 1);
-	//here 행 안뒤집었을 때
-	coin[here] = ~coin[here];
-	solution(here + 1);
+	//cur 행 뒤집기
+	arr[cur] = ~arr[cur];
+	dfs(cur + 1);
+
+	//뒤집지 않았을 때
+	arr[cur] = ~arr[cur];
+	dfs(cur + 1);
 }
 
 
 int main()
 {
 	cin >> N;
-	for (int i = 1; i <= N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		cin >> str;
 		int value = 1;
+		cin >> str;
 		for (int j = 0; j < str.size(); j++)
 		{
-			if (str[j] == 'T')coin[i] |= value;
+			if (str[j] == 'T')arr[i] |= value;
 			value *= 2;
 		}
-
 	}
-	solution(1);
-	cout << ret << '\n';
-
+	dfs(0);
+	cout << ret;
 
 	return 0;
 }
