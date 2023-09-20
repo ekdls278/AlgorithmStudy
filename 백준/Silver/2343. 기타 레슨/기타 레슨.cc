b4f5263a4730c43ad745c3fdc2bad871ddeb901b
KimDain <80109656+ekdls278@users.noreply.github.com>
2023-09-20 22:03:51 +0900

    [Silver I] Title: 기타 레슨, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -0,0 +1,71 @@
+#include<iostream>
+#pragma warning(disable:4996)
+#define MAX 100001
+using namespace std;
+
+int N, M;
+int arr[MAX];
+int sum;
+int ret = 987654321;
+
+
+void Input()
+{
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+		sum += arr[i];
+	}
+}
+
+bool check(int mid)
+{
+	int cnt = 1;
+	int tmpSum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (arr[i] > mid)return 0;
+		tmpSum += arr[i];
+		if (tmpSum > mid)
+		{
+			cnt++;
+			tmpSum = arr[i];
+		}
+	}
+	return cnt <= M;
+
+
+}
+
+
+void BinarySearch()
+{
+	int l = 1, r = sum;
+	while (l <= r)
+	{
+		int mid = (l + r) / 2;
+		if (check(mid))
+		{
+			ret = min(ret, mid);
+			r = mid - 1;
+		}
+		else
+		{
+			l = mid + 1;
+		}
+	}
+}
+
+int main()
+{
+#ifdef _DEBUG
+	freopen("C:\\Users\\ekdls\\Desktop\\study\\2343.txt", "r", stdin);
+#endif
+	Input();
+	BinarySearch();
+	cout << ret;
+
+
+	return 0;
+}
